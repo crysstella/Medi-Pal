95e8a6198fd2a4981e6138b7f4113eca99dd349e
crysstella <smile461999@gmail.com>
2024-04-04 18:48:43 -0700

    Edit notification.dart.

--- lib/pages/Notification/notification.dart
+++ lib/pages/Notification/notification.dart
@@ -39,27 +39,28 @@ class NotificationsState extends State<Notifications> {
   }
 }
 
-/*
-import 'package:flutter/material.dart';
-
-class Notifications extends StatefulWidget {
-  const Notifications({super.key});
-
-  @override
-  State<Notifications> createState() => NotificationsState();
-}
-
-class NotificationsState extends State<Notifications> {
-  @override
+/*@override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body:  Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text('Notifications')]),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.all(8.0),
+            child: ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notification Title $index'),
+              subtitle: Text('This is a description of the notification.'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                // Handle the tap
+              },
+            ),
+          );
+        },
       ),
     );
-  }
-}
-
-*/
+  }*/
