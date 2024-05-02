c95b298d2d7fe03aa88d2e5614c2fcd41d150f2f
crysstella <smile461999@gmail.com>
2024-05-01 20:43:45 -0700

    update notification page

--- lib/pages/Notification/notification.dart
+++ lib/pages/Notification/notification.dart
@@ -5,6 +5,12 @@ import '../Schedule/event.dart';
 import 'localNotification.dart';
 import 'notification_details.dart';
 
+class MyNotification extends Notification {
+  final String message;
+
+  MyNotification(this.message);
+}
+
 class Notifications extends StatefulWidget {
   const Notifications({super.key});
 
@@ -14,14 +20,15 @@ class Notifications extends StatefulWidget {
 
 class NotificationsState extends State<Notifications> {
   static List<Event> events = [];
+  /*static ValueNotifier<List<Event>> notifiedEvents =
+      ValueNotifier<List<Event>>([]);*/
 
   @override
   void initState() {
     super.initState();
-    /*LocalNotifications.notificationStreamController.stream
-        .listen((String event) {
+    LocalNotifications.notificationStream.listen((String event) {
       handleNotification(event);
-    });*/
+    });
     //listenToNotification();
     //notificationReceived();
   }
@@ -29,71 +36,65 @@ class NotificationsState extends State<Notifications> {
   // Handle notification and adding event to page
   void handleNotification(String event) {
     setState(() {
-      Event _event = Event.deserialize(event);
-      events.add(_event);
+      Event eventTemp = Event.deserialize(event);
+      events.add(eventTemp);
       print('length = ${events.length}');
     });
   }
 
-  /*// Listen to any notification
-  notificationReceived(){
-    LocalNotifications.onNotificationStream.stream.listen((String? event) {
-      if (event != null){
-        handleNotification(event);
-      }
-    });
-  }*/
-
-  // Listen to any notification clicked or not
-  /*listenToNotification() {
-    LocalNotifications.onClickNotification.stream.listen((String event) {
-      handleNotification(event);
-    });
-  }*/
-
-  /* @override
-  void dispose(){
-    LocalNotifications.onNotificationStream.close();
+  /*@override
+  void dispose() {
+    notifiedEvents.dispose();
+    LocalNotifications.dispose();
+    super.dispose();
   }*/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: events.length,
-        itemBuilder: (context, index) {
-          print('EVENT LENGTH = ${events.length}');
-          Event event = events[index];
-          return Dismissible(
-              key: Key(event.hashCode
-                  .toString()), // Use a unique key for Dismissible
-              background: Container(color: Colors.red),
-              onDismissed: (direction) {
-                // Remove the item from the list
-                setState(() {
-                  events.removeAt(index);
-                });
+        body: ListView.builder(
+            itemCount: events.length,
+            itemBuilder: (context, index) {
+              return ListView.separated(
+                padding: const EdgeInsets.all(8),
+                itemCount: events.length,
+                itemBuilder: (context, index) {
+                  print('EVENT LENGTH = ${events.length}');
+                  print('EVENT = ${events}');
+                  Event event = events[index];
+                  return Dismissible(
+                      key: Key(event.hashCode
+                          .toString()), // Use a unique key for Dismissible
+                      background: Container(color: Colors.red),
+                      /*onDismissed: (direction) {
+                            // Remove the item from the list
+                            setState(() {
+                              notifiedEvents.value.removeAt(index);
+                            });
 
-                // Show a snackbar This snackbar could also contain "Undo" actions.
-                ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Reminder dismissed')));
-              },
-              child: Card(
-                margin: EdgeInsets.all(8.0),
-                child: ListTile(
-                  title: Text('Medicine Reminder at ${event.getTime(context)}'),
-                  subtitle: Text("It's time to take ${event.medicine}."),
-                  trailing: const Icon(UniconsLine.angle_right),
-                  onTap: () {
-                    // Handle the tap
-                  },
-                ),
-              ));
-        },
-        separatorBuilder: (context, index) => const SizedBox(),
-      ),
-      /*body: ListView.builder(
+                            // Show a snackbar This snackbar could also contain "Undo" actions.
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Reminder dismissed')));
+                          },*/
+                      child: Card(
+                        margin: EdgeInsets.all(8.0),
+                        child: ListTile(
+                          title: Text(
+                              'Medicine Reminder at ${events[index].getTime(context)}'),
+                          subtitle: Text(
+                              "It's time to take ${events[index].medicine}."),
+                          trailing: const Icon(UniconsLine.angle_right),
+                          onTap: () {
+                            // Handle the tap
+                          },
+                        ),
+                      ));
+                },
+                separatorBuilder: (context, index) => const SizedBox(),
+              );
+            }));
+
+    /*body: ListView.builder(
         itemCount: events.length,
         itemBuilder: (context, index) {
           Event event = events[index];
@@ -110,6 +111,5 @@ class NotificationsState extends State<Notifications> {
           );
         },
       ),*/
-    );
   }
 }
