b145f2debffdb99780804f5af2e3685ae2d58333
crysstella <smile461999@gmail.com>
2024-05-11 14:19:55 -0700

    add Notification Bloc for StartApp to update index when notification tapped.

--- lib/pages/Start/startApp.dart
+++ lib/pages/Start/startApp.dart
@@ -1,11 +1,8 @@
-
+import 'package:bloc_notification/bloc_notification.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get/get.dart';
-import 'package:medipal/pages/Notification/notification.dart';
+import 'package:medipal/blocs/notification_bloc/notification_bloc.dart';
 import 'package:unicons/unicons.dart';
 import 'package:medipal/components/drawer/NavigationItem.dart';
-import '../../blocs/log_in_bloc/log_in_bloc.dart';
 import '../Notification/localNotification.dart';
 
 /* Start page contains navigation bars*/
@@ -27,17 +24,11 @@ class StartAppState extends State<StartApp> {
   LastTappedBar? _lastTappedBar; // Check if the current tap is bottom or side
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    LocalNotifications.indexUpdateController.stream.listen((newIndex) {
-      setState(() {
-        print('NEW INDEX = $newIndex');
-        _onItemTappedBottom(newIndex);
-        print('CURRENT INDEX = $currentIndex');
-
-      });
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      LocalNotifications.init(context);
     });
-
   }
 
   // Set the index of the bottom bar
@@ -96,130 +87,127 @@ class StartAppState extends State<StartApp> {
 
   @override
   Widget build(BuildContext context) {
-    //NotiController notiController = Get.put(NotiController());
     final safeArea =
         EdgeInsets.only(top: MediaQuery.of(context).viewPadding.top);
-    return Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(
-          title: currentIndex == -1
-              ? Text(
-                  NavigationItem.items[currentIndexSide].title,
-                  style: const TextStyle(color: Colors.white, fontSize: 18),
-                )
-              : Text(
-                  NavigationItem.items[currentIndex].title,
-                  style: const TextStyle(color: Colors.white, fontSize: 18),
-                ),
-          backgroundColor: Theme.of(context).primaryColor,
-          leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(
-                  UniconsLine.bars,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  _scaffoldKey.currentState?.openDrawer();
+    return BlocNotificationListener<NotificationBloc, NotificationState,
+            MyNotifications>(
+        notificationListener: (context, notification) {
+          if (notification is UpdateNotificationPageIndex) {
+            _onItemTappedBottom(2);
+          }
+        },
+        child: Scaffold(
+            key: _scaffoldKey,
+            appBar: AppBar(
+              title: currentIndex == -1
+                  ? Text(
+                      NavigationItem.items[currentIndexSide].title,
+                      style: const TextStyle(color: Colors.white, fontSize: 18),
+                    )
+                  : Text(
+                      NavigationItem.items[currentIndex].title,
+                      style: const TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+              backgroundColor: Theme.of(context).primaryColor,
+              leading: Builder(
+                builder: (BuildContext context) {
+                  return IconButton(
+                    icon: const Icon(
+                      UniconsLine.bars,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      _scaffoldKey.currentState?.openDrawer();
+                    },
+                    tooltip:
+                        MaterialLocalizations.of(context).openAppDrawerTooltip,
+                  );
                 },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            },
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  //notiController.requestForPermission();
-                  //context.read<LogInBloc>().add(const LogOutRequired());
-                },
-                icon: Icon(
-                  UniconsLine.x,
-                  color: Theme.of(context).colorScheme.onPrimary,
-                ))
-          ],
-        ),
-        drawer: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.2,
-            child: Drawer(
-                child: Column(children: [
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 24).add(safeArea),
-                width: double.infinity,
-                child: barHeader(context),
               ),
-              const Divider(color: Colors.black12),
-              const SizedBox(height: 10),
-              Wrap(runSpacing: 20, children: <Widget>[
-                IconButton(
-                  icon: const Icon(UniconsLine.schedule, size: 38),
-                  onPressed: () {
-                    _onItemTappedDrawer(4);
-                    print(currentIndexSide);
-                    print(currentIndex);
-                  },
-                  isSelected: (currentIndexSide == 4 && currentIndex == -1)
-                      ? true
-                      : false,
-                ),
-                IconButton(
-                  icon: const Icon(UniconsLine.comment_alt, size: 38),
-                  onPressed: () {
-                    _onItemTappedDrawer(5);
-                    print(currentIndexSide);
-                    print(currentIndex);
-                  },
-                  isSelected: (currentIndexSide == 5 && currentIndex == -1)
-                      ? true
-                      : false,
-                ),
-                IconButton(
-                  icon: const Icon(UniconsLine.heart, size: 38),
-                  onPressed: () {
-                    _onItemTappedDrawer(6);
-                    print(currentIndexSide);
-                    print(currentIndex);
-                  },
-                  isSelected: (currentIndexSide == 6 && currentIndex == -1)
-                      ? true
-                      : false,
-                )
-              ]),
-              const Divider(color: Colors.black12),
-              const SizedBox(height: 10),
-              barInfoMenu(context),
-              const Spacer()
-            ]))),
-        // navigate the body page into the new page based on the tap
-        body: Center(
-          child: (_lastTappedBar == LastTappedBar.bottomNavigationBar ||
-                  _lastTappedBar == null)
-              ? _getBodyWidget(currentIndex)
-              : _getBodyWidget(currentIndexSide),
-        ),
-        // Bottome navigation bar.
-        bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            iconSize: 38,
-            items: const <BottomNavigationBarItem>[
-              // Home
-              BottomNavigationBarItem(
-                  icon: Icon(UniconsLine.home), label: 'Home'),
-              // Search
-              BottomNavigationBarItem(
-                  icon: Icon(UniconsLine.search), label: 'Search'),
-              // Notifications
-              BottomNavigationBarItem(
-                  icon: Icon(UniconsLine.bell), label: 'Notifications'),
-            ],
-            /* Keep track which icon is tapped at the bottom bar.
+            ),
+            drawer: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.2,
+                child: Drawer(
+                    child: Column(children: [
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 24).add(safeArea),
+                    width: double.infinity,
+                    child: barHeader(context),
+                  ),
+                  const Divider(color: Colors.black12),
+                  const SizedBox(height: 10),
+                  Wrap(runSpacing: 20, children: <Widget>[
+                    IconButton(
+                      icon: const Icon(UniconsLine.schedule, size: 38),
+                      onPressed: () {
+                        _onItemTappedDrawer(4);
+                        print(currentIndexSide);
+                        print(currentIndex);
+                      },
+                      isSelected: (currentIndexSide == 4 && currentIndex == -1)
+                          ? true
+                          : false,
+                    ),
+                    IconButton(
+                      icon: const Icon(UniconsLine.comment_alt, size: 38),
+                      onPressed: () {
+                        _onItemTappedDrawer(5);
+                        print(currentIndexSide);
+                        print(currentIndex);
+                      },
+                      isSelected: (currentIndexSide == 5 && currentIndex == -1)
+                          ? true
+                          : false,
+                    ),
+                    IconButton(
+                      icon: const Icon(UniconsLine.heart, size: 38),
+                      onPressed: () {
+                        _onItemTappedDrawer(6);
+                        print(currentIndexSide);
+                        print(currentIndex);
+                      },
+                      isSelected: (currentIndexSide == 6 && currentIndex == -1)
+                          ? true
+                          : false,
+                    )
+                  ]),
+                  const Divider(color: Colors.black12),
+                  const SizedBox(height: 10),
+                  barInfoMenu(context),
+                  const Spacer()
+                ]))),
+            // navigate the body page into the new page based on the tap
+            body: Center(
+              child: (_lastTappedBar == LastTappedBar.bottomNavigationBar ||
+                      _lastTappedBar == null)
+                  ? _getBodyWidget(currentIndex)
+                  : _getBodyWidget(currentIndexSide),
+            ),
+            // Bottome navigation bar.
+            bottomNavigationBar: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                iconSize: 38,
+                items: const <BottomNavigationBarItem>[
+                  // Home
+                  BottomNavigationBarItem(
+                      icon: Icon(UniconsLine.home), label: 'Home'),
+                  // Search
+                  BottomNavigationBarItem(
+                      icon: Icon(UniconsLine.search), label: 'Search'),
+                  // Notifications
+                  BottomNavigationBarItem(
+                      icon: Icon(UniconsLine.bell), label: 'Notifications'),
+                ],
+                /* Keep track which icon is tapped at the bottom bar.
              * If not the last item at the bottom was
              * tapped still being selected with another pages. */
-            currentIndex: currentIndex == -1 ? 0 : currentIndex,
-            selectedItemColor: currentIndex == -1
-                ? Colors.grey[600]
-                : Theme.of(context).primaryColor,
-            showSelectedLabels: currentIndex == -1 ? false : true,
-            showUnselectedLabels: false,
-            onTap: _onItemTappedBottom));
+                currentIndex: currentIndex == -1 ? 0 : currentIndex,
+                selectedItemColor: currentIndex == -1
+                    ? Colors.grey[600]
+                    : Theme.of(context).primaryColor,
+                showSelectedLabels: currentIndex == -1 ? false : true,
+                showUnselectedLabels: false,
+                onTap: _onItemTappedBottom)));
   }
 }
