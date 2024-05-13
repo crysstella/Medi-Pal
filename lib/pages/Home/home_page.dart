266b295d0dffed70028a96f349b8718927263c94
easymac60 <omgitzmeexx@gmail.com>
2024-05-12 21:29:13 -0700

    fix home page

--- lib/pages/Home/home_page.dart
+++ lib/pages/Home/home_page.dart
@@ -4,7 +4,6 @@ import '../../pages/Profile/profileInput.dart';
 import '../../sharedPref.dart';
 import '../HealthAssessment/inputScreen.dart';
 
-
 class HomePage extends StatefulWidget {
   //static const String routeName = "/homePage";
   const HomePage({super.key});
@@ -14,9 +13,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   late Future<String?> email;
-    late Future<String?> name;
+  late Future<String?> name;
   String? userEmail;
   String? userName;
 
@@ -40,81 +38,80 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-
     Widget _header() {
       Widget _btnEdit(
           {required String title,
           required IconData icon,
           required Function onTap,
           required Color backgroundcolor}) {
-          return Flexible(
-            child: TextButton(
-              onPressed: () => onTap(),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(icon, size: 20, color: Colors.black),
-                  const SizedBox(width: 4.0),
-                  Text(
-                    title,
-                    style: const TextStyle(color: Colors.black, fontSize: 16),
-                  ),
-                ],
-              ),
-              style: ButtonStyle(
-                fixedSize: MaterialStateProperty.all(
-                    Size(MediaQuery.of(context).size.width * 0.40, 62)),
-                padding: MaterialStateProperty.all(
-                    const EdgeInsets.only(left: 12, right: 12, top: 12, bottom: 12)),
-                backgroundColor:
-                    MaterialStateProperty.all(backgroundcolor), //Background Color
-                elevation: MaterialStateProperty.all(4), //Defines Elevation
-                shadowColor: MaterialStateProperty.all(Colors.grey), //Define
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32.0),
-                  ),
+        return Flexible(
+          child: TextButton(
+            onPressed: () => onTap(),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon, size: 20, color: Colors.black),
+                const SizedBox(width: 4.0),
+                Text(
+                  title,
+                  style: const TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ],
+            ),
+            style: ButtonStyle(
+              fixedSize: MaterialStateProperty.all(
+                  Size(MediaQuery.of(context).size.width * 0.40, 62)),
+              padding: MaterialStateProperty.all(const EdgeInsets.only(
+                  left: 12, right: 12, top: 12, bottom: 12)),
+              backgroundColor:
+                  MaterialStateProperty.all(backgroundcolor), //Background Color
+              elevation: MaterialStateProperty.all(4), //Defines Elevation
+              shadowColor: MaterialStateProperty.all(Colors.grey), //Define
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32.0),
                 ),
               ),
             ),
+          ),
         );
       }
 
       return Container(
-        padding: const EdgeInsets.only(
-            left: 16,
-            right: 16,
-            bottom: 16),
+        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
         decoration: BoxDecoration(
             color: Theme.of(context).primaryColor,
             borderRadius: const BorderRadius.only(
                 bottomLeft: Radius.circular(0),
                 bottomRight: Radius.circular(0))),
-        
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(width: 2),
             Text(
-              //"Welcome ! ", 
-              'Welcome $userName !',
-              style: TextStyle(color: Theme.of(context).colorScheme.onPrimary, fontSize: 30, fontWeight: FontWeight.w900),
+              "Welcome! ", //link user, add bloc listener
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900),
             ),
             const SizedBox(height: 12),
             Text(
               "Keep your information up to date to get the most of your services. You can edit your profile or make changes to your health history here.",
-              style: TextStyle(color: Theme.of(context).colorScheme.onPrimary, fontSize: 16),
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary, fontSize: 16),
             ),
             const SizedBox(height: 14),
             Text(
               "New? Add your information now to access your profile.",
-              style: TextStyle(color: Theme.of(context).colorScheme.onPrimary, fontSize: 14, fontWeight: FontWeight.w100),
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w100),
             ),
             const SizedBox(height: 18),
             Row(
@@ -123,25 +120,26 @@ class _HomePageState extends State<HomePage> {
                 _btnEdit(
                     title: "Edit Profile",
                     icon: UniconsLine.history,
-                    backgroundcolor: Theme.of(context).colorScheme.primaryContainer,
-                      onTap: () async {
-                        Navigator.push(
+                    backgroundcolor:
+                        Theme.of(context).colorScheme.primaryContainer,
+                    onTap: () async {
+                      Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => ProfileInput(email: email),
-                       ),
-                     );
+                        ),
+                      );
                     }),
-
                 _btnEdit(
                     title: "Add Disease",
-                    backgroundcolor: Theme.of(context).colorScheme.tertiaryContainer,
+                    backgroundcolor:
+                        Theme.of(context).colorScheme.tertiaryContainer,
                     icon: UniconsLine.plus,
                     onTap: () {
                       Navigator.push(
-                       context,
-                       MaterialPageRoute(
-                         builder: (context) => InputScreen(email: userEmail),
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => InputScreen(email: userEmail),
                         ),
                       );
                     })
@@ -153,38 +151,29 @@ class _HomePageState extends State<HomePage> {
       );
     }
 
-
-
     Widget _center() {
-
       Widget __helpItem({required String title}) {
-
-
         return Expanded(
           child: Column(
             children: [
               const SizedBox(height: 12),
-              
-              TextButton(              
-                onPressed: () {  },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(                
-                      title,
-                      textAlign: TextAlign.center,
-                      maxLines: 3,
-                      style: const TextStyle(color: Colors.black, fontSize: 14))
-
-                  ],)
-
-                ),
+              TextButton(
+                  onPressed: () {},
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(title,
+                          textAlign: TextAlign.center,
+                          maxLines: 3,
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 14))
+                    ],
+                  )),
             ],
           ),
         );
-
-    }
+      }
 
       return Container(
         padding: const EdgeInsets.all(16),
@@ -193,11 +182,13 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             const SizedBox(height: 8),
-            Text("MEDI-PAL WILL HELP WITH YOUR HEALTH",style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: 18, fontWeight: FontWeight.bold)),
+            Text("GET HELP WITH YOUR HEALTH",
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold)),
             const SizedBox(height: 8),
             SizedBox(
-
-              
               // height: MediaQuery.of(context).size.width / 3,
               width: MediaQuery.of(context).size.width,
               child: Row(
@@ -215,10 +206,8 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
-
-
             ),
-            const SizedBox(height: 110),
+            const SizedBox(height: 140),
           ],
         ),
       );
@@ -226,41 +215,35 @@ class _HomePageState extends State<HomePage> {
 
     Widget _bottom() {
       return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Stack(
+        decoration: BoxDecoration(
+            //color: Theme.of(context).colorScheme.tertiary.withOpacity(0.8),
+            gradient: LinearGradient(colors: [
+              Theme.of(context).colorScheme.tertiary.withOpacity(0.9),
+              Theme.of(context).colorScheme.tertiary.withOpacity(0.6),
+            ], begin: Alignment.topRight, end: Alignment.center),
+            borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              margin: const EdgeInsets.only(top: 24.0),
-              padding: EdgeInsets.only(
-                  left: 12,
-                  right: 12,
-                  top: 12,
-                  bottom: 12),
-              decoration: BoxDecoration(
-                //color: Theme.of(context).colorScheme.tertiary.withOpacity(0.8),
-                  gradient: LinearGradient(colors: [
-                    Theme.of(context).colorScheme.tertiary.withOpacity(0.9),
-                    Theme.of(context).colorScheme.tertiary.withOpacity(0.6),
-                  ], begin: Alignment.topRight, end: Alignment.center),
-                  borderRadius: BorderRadius.circular(10)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 4),
-                  Text(
-                    "Questions?",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onSecondary, fontSize: 16, fontWeight: FontWeight.w100),
-                  ),
-                   const SizedBox(height: 4),
-                  Text(
-                    "Read our privacy policy and learn more about us under our SUPPORT located in your app settings.",
-                    style: TextStyle(color: Theme.of(context).colorScheme.onTertiary, fontSize: 13.5),
-                  ),
-                  const SizedBox(height: 6),
-                ],
-              ),
+            const SizedBox(height: 4),
+            Text(
+              "Questions?",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.onSecondary,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w100),
             ),
+            const SizedBox(height: 4),
+            Text(
+              "Read our privacy policy and learn more about us under our SUPPORT located in your app settings.",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.onTertiary,
+                  fontSize: 13.5),
+            ),
+            const SizedBox(height: 6),
           ],
         ),
       );
@@ -269,15 +252,11 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Center(
         child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [_header(), _center(), _bottom()],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [_header(), _center(), _bottom()],
+        ),
       ),
-    ),
-  ); 
-
-
-
-
+    );
   }
 }
